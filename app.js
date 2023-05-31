43eb8d5b61ea8afbd43ca5cea52897c745000fdc
Leonardo Amador <amadorleonardo8109@gmail.com>
2023-05-30 21:04:54 -0300

    Created a test server.

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send('<h1>My task list</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Sever running');
+});
